d31dd17f8287d789b262046c277b00d34f83465c
JohnBadri <135007003+JohnBadri@users.noreply.github.com>
2023-11-01 22:35:41 +1100

    Final Product - The Odin Project - To Do List - Final Production - Changed Name

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Webpack App",
+      title: "TOP - To Do List",
       filename: "index.html",
       template: "src/index.html",
     }),
